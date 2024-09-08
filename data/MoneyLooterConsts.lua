8511fbdeb88a7a9495ca6582f28e31155f1ba9f5
Guillermo López Rodríguez <guillermin97@gmail.com>
2024-09-08 23:13:58 +0200

    fix: bad item parse, mixing name with quantity rearranged patterns

--- data/MoneyLooterConsts.lua
+++ data/MoneyLooterConsts.lua
@@ -34,17 +34,17 @@ ML_EVENTS = {
 -- Loot global patterns for self
 -- string.match returns itemLink, quantity || itemLink
 LOOT_PATTERNS_SELF = {
-    LOOT_ITEM_SELF:gsub("%%s", "(.+)"),                                      -- 1
-    LOOT_ITEM_SELF_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"),       -- 2
-    LOOT_ITEM_PUSHED_SELF:gsub("%%s", "(.+)"),                               -- 1
-    LOOT_ITEM_PUSHED_SELF_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)") -- 2
+    LOOT_ITEM_SELF_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"),        -- 2
+    LOOT_ITEM_PUSHED_SELF_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"), -- 2
+    LOOT_ITEM_SELF:gsub("%%s", "(.+)"),                                       -- 1
+    LOOT_ITEM_PUSHED_SELF:gsub("%%s", "(.+)")                                 -- 1
 }
 
 -- Loot global patterns for others
 -- string.match returns playerName, itemLink, quantity || playerName, itemLink
 LOOT_PATTERNS = {
-    LOOT_ITEM:gsub("%%s", "(.+)"),                                      -- 2
-    LOOT_ITEM_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"),       -- 3
-    LOOT_ITEM_PUSHED:gsub("%%s", "(.+)"),                               -- 2
-    LOOT_ITEM_PUSHED_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)") -- 3
+    LOOT_ITEM_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"),        -- 3
+    LOOT_ITEM_PUSHED_MULTIPLE:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)"), -- 3
+    LOOT_ITEM:gsub("%%s", "(.+)"),                                       -- 2
+    LOOT_ITEM_PUSHED:gsub("%%s", "(.+)")                                 -- 2
 }
