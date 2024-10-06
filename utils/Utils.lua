49f8547efad7480a05678608bb14990e40f7b19d
Guillermo López Rodríguez <guillermin97@gmail.com>
2024-10-06 19:46:42 +0200

    fix: money display

--- utils/Utils.lua
+++ utils/Utils.lua
@@ -17,14 +17,14 @@ end
 ---@param val integer
 ---@return string
 function Utils.GetCoinTextString(val)
-    if val == nil then return "0|cFFC07A50c|r" end
+    if val == nil or val == 0 then return "00|cFFC07A50c|r" end
     local gold = math.floor(val / 10000)
     local silver = math.floor(val % 10000 / 100)
     local copper = math.floor(val % 100)
 
     local fString = ""
     if gold > 0 then fString = fString .. gold .. "|cFFD9BE4Cg|r" end
-    if silver > 0 then fString = fString .. string.format("%02u|cFFB3B4BAs|r", silver) end
+    if gold > 0 or silver > 0 then fString = fString .. string.format("%02u|cFFB3B4BAs|r", silver) end
     fString = fString .. string.format("%02u|cFFC07A50c|r", copper)
     return fString
 end
