390896fcdf11507416be2c3422e604a0d90608c2
Guillermo López Rodríguez <guillermin97@gmail.com>
2024-10-13 17:47:56 +0200

    deleted unused summary boolean

--- data/LootedItem.lua
+++ data/LootedItem.lua
@@ -9,14 +9,12 @@ MoneyLooter.LootedItem = LootedItem
 ---@param itemLink string
 ---@param value integer
 ---@param quantity integer
----@param isSummary boolean?
-function LootedItem.new(id, itemLink, value, quantity, isSummary)
+function LootedItem.new(id, itemLink, value, quantity)
     local self = {}
     self.id = id
     self.itemLink = itemLink
     self.value = value
     self.quantity = quantity
-    self.isSummary = isSummary or false
 
     return self
 end
